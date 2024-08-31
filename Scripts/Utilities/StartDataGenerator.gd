f6904b69145eadd44964edca41e77f5a2a970acb
CeffreyJostello <90864809+CeffreyJostello@users.noreply.github.com>
2024-08-30 23:59:08 -0500

    Added Dice Ids

--- Scripts/Utilities/StartDataGenerator.gd
+++ Scripts/Utilities/StartDataGenerator.gd
@@ -3,9 +3,11 @@ extends Node
 class_name StarterDataGenerator
 var constants = load('res://GameData/Constants.gd').new()
 var idGenerator = IdGenerator.new()
+
 func generate():
 	var start_data = constants.START_DATA_FORMAT
 	for die in start_data['dice']:
+		die['id'] = idGenerator.generateGuid()
 		for face in die['faces']:
 			face['id'] = idGenerator.generateGuid()
 	return start_data
