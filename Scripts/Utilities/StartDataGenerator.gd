2080ff5f2ff646320da72e69a79c3c34f2e3ea45
CeffreyJostello <90864809+CeffreyJostello@users.noreply.github.com>
2024-09-11 13:00:28 -0500

    Fixed Data Format and Start Generator

--- Scripts/Utilities/StartDataGenerator.gd
+++ Scripts/Utilities/StartDataGenerator.gd
@@ -7,5 +7,16 @@ var idGenerator = IdGenerator.new()
 func generate():
 	var start_data = constants.START_DATA_FORMAT
 	var dice_dupe = start_data['dice'].duplicate()
+	var idGenerator = IdGenerator.new()
 	start_data['dice'] = {}
+	for die in dice_dupe:
+		var face_dupe = die['faces'].duplicate()
+		die['faces'] = {}
+		for face in face_dupe:
+			var faceId = idGenerator.generateGuid()
+			die['faces'][faceId] = face
+		var diceId = idGenerator.generateGuid()
+		start_data['dice'][diceId] = die
+		start_data['dice'][diceId] = die
+		
 	return start_data
